c704f0798b22aec3245689588b5addf1d0cff2e0
taiyuanhong <tai.yuanhong@etaostars.com>
2015-11-10 18:04:58 +0800

    2region

---
+++ ESend/ESend/VC/Main/9CellViews/HpSecondaryRegionView.h
@@ -0,0 +1,13 @@
+//
+//  HpSecondaryRegionView.h
+//  ESend
+//
+//  Created by 台源洪 on 15/11/10.
+//  Copyright © 2015年 Saltlight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HpSecondaryRegionView : UIView
+
+@end
---
+++ ESend/ESend/VC/Main/9CellViews/HpSecondaryRegionView.m
@@ -0,0 +1,19 @@
+//
+//  HpSecondaryRegionView.m
+//  ESend
+//
+//  Created by 台源洪 on 15/11/10.
+//  Copyright © 2015年 Saltlight. All rights reserved.
+//
+
+#import "HpSecondaryRegionView.h"
+
+@implementation HpSecondaryRegionView
+- (instancetype)init{
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+@end
