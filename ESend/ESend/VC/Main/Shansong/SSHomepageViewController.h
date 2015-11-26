3844b5743ca0a9423cb77821846a9a968e7a8af6
taiyuanhong <tai.yuanhong@etaostars.com>
2015-11-26 11:52:02 +0800

    ssUI

---
+++ ESend/ESend/VC/Main/Shansong/SSHomepageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSHomepageViewController.h
+//  ESend
+//
+//  Created by 台源洪 on 15/11/25.
+//  Copyright © 2015年 Saltlight. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SSHomepageViewController : BaseViewController
+
+@end
