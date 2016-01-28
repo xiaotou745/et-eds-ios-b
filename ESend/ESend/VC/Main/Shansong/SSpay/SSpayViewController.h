54d705acbebae09fd00fc8a86daade3a5c74bb45
taiyuanhong <tai.yuanhong@etaostars.com>
2016-01-28 14:59:18 +0800

    remark

--- ESend/ESend/VC/Main/Shansong/SSpay/SSpayViewController.h
+++ ESend/ESend/VC/Main/Shansong/SSpay/SSpayViewController.h
@@ -11,10 +11,9 @@
 @interface SSpayViewController : BaseViewController
 
 @property (nonatomic,strong) NSString * orderId;
-@property (nonatomic,assign) NSInteger type;    // 1发单，2抢单之前，待支付订单列表
-@property (nonatomic,assign) double tipAmount;  // 支付金额,只是小费（！！！）
+@property (nonatomic,assign) NSInteger type;    // 1发单，2加小费
+@property (nonatomic,assign) double tipAmount;  // 支付金额
 @property (nonatomic,assign) double balancePrice;  // 账户余额
-
-@property (nonatomic,strong) NSString * pickupcode;
+@property (nonatomic,strong) NSString * pickupcode; // 取货码
 
 @end
